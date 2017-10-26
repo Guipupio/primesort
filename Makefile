296a8c7a33df2e366e5b7e2e39b4d81c61c1e78f
guipupio <ra168958@le27-19.grad.fee.unicamp.br>
2017-10-26 13:47:38 -0200

    funcao primo otimizada; Nao funciona o pipe

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Macros para compilacao
 CC = gcc
-CFLAGS = -Wextra
+CFLAGS = -Wextra -std=gnu99
 DIR = src
 FILENAME = $(DIR)/main.c
 TARGET = ./main
--- src/main.c
+++ src/main.c
@@ -1,17 +1,66 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+
+#define True 1
+#define False 0
+#define FIM -1
+
+
+uint8_t eh_primo(int num){
+
+
+	
+	if (num>3){
+    if (num % 2 == 0) return False;
+		for (int i =3; i*i<=num; i+=1)
+			if(num % i == 0)
+				return False;
+	return True;
+	} else if (num == 2 || num == 3)
+       return True;
+    else
+  	   return False;	
+}
 
 int main() {
 
-  int x, y;
+  int i = 0, j = 0, entrada = 0;
+  uint8_t flag_fim_leitura = False;
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+  FILE *txt;
+  FILE *pipein;
+  FILE *pipeout;
+
+  pipein = popen("> auxiliar","w");
+
+  if((txt = fopen("auxiliar","w") ) == NULL){
+  	printf("DEU MERDA 1\n");
+  	exit(1);
+  }
+
+  while (!flag_fim_leitura){
+  	scanf("%d",&entrada);
+  	if(entrada == FIM)
+  		flag_fim_leitura = True;
+  	else{
+  		if (!eh_primo(entrada)){
+  			fprintf(txt, "%d\n",entrada);
+  		i++;
+  		}
+  	 	
+  	}
+  }
+
+ pipeout = popen("sort -n -r auxiliar", "r");
+ 
+  // i = 0;
+  // while (fscanf(txt,"%d\n",&entrada[i]) != EOF)
+  // 	i++;
+
+  pclose(pipein);
+  pclose(pipeout);
+  free(txt);
   return 0;
 }
